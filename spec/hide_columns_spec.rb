a7bf7097a3639561c5956d64587ebae63058f95a
Wong Liang Zan <zan@liangzan.net>
2009-12-15 17:19:11 +0800

    added the specs

--- spec/hide_columns_spec.rb
+++ spec/hide_columns_spec.rb
@@ -1,7 +1,35 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "HideColumns" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+
+  before(:each) do
+    @original_content_cols = [ mock_active_record("house"), mock_active_record("car"), mock_active_record("room"), mock_active_record("television") ]
+    MockedModel.stubs(:content_columns).returns(@original_content_cols)
   end
+
+  def mock_active_record(name)
+    mock_active_record = mock()
+    mock_active_record.stubs(:name).returns(name)
+    mock_active_record
+  end
+
+  def hide_cols(cols)
+    MockedModel.send(:hide_columns, cols)
+  end
+
+  def should_not_have_cols(hidden_cols)
+    hidden_cols.each do |hidden_col|
+      MockedModel.content_columns.each do |column|
+        /^#{hidden_col}/.match(column.name).should be_nil
+      end
+    end
+  end
+
+  # Mocked Model will persist the hidden columns in the same set of specs
+  it "should hide content columns if the selected column match exactly" do
+    hidden_cols = %w(house car plane)
+    hide_cols(hidden_cols)
+    should_not_have_cols(hidden_cols)
+  end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,17 @@
+require 'spec'
+require 'spec/autorun'
+require 'rubygems'
+require 'mocha'
+require 'active_support'
+require 'active_record'
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'hide_columns'
-require 'spec'
-require 'spec/autorun'
 
 Spec::Runner.configure do |config|
-  
+
+end
+
+class MockedModel
+  include HideColumns
 end
